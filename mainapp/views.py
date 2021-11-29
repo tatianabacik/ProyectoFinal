5534f9ede1d900b07ba0d319e8155a44bd9c1316
CristianGuerra <camguerra93@gmail.com>
2021-11-29 19:30:38 -0300

    Models v1

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.http import response
+from django.shortcuts import render, HttpResponse, redirect
+from mainapp.models import Article
 
 # Create your views here.
-
-layout = '''<h1>Sitio web con Django | Cristian Guerra</h1>'''
 
 def index(request):
     return render(request, 'mainapp/index.html')
@@ -12,3 +12,55 @@ def pagina2(request):
 
 def pagina3(request):
     return render(request, 'mainapp/pagina3.html')
+
+def crear_articulo(request):
+    articulo = Article(
+        title = 'Segundo articulo',
+        content = 'Contenido del segundo articulo',
+        public = True
+    )
+    articulo.save()
+    return HttpResponse(f'Articulo Creado: {articulo.title}')
+
+def articulo(request):
+    try:
+        articulo = Article.objects.get(title = 'Primer articulo')
+        response = f'Mostrando Articulo: {articulo.title}'
+    except:
+        response = 'Articulo no encontrado'
+
+    return HttpResponse(response)
+
+def editar_articulo(request, id):
+    articulo = Article.objects.get(pk=id)
+    articulo.title = 'Primer Articulo Modificado'
+    articulo.content = 'Contenido modificado del primer articulo'
+    articulo.public = True
+
+    articulo.save()
+    return HttpResponse(f'Articulo Editado: {articulo.title} <br>{articulo.content}')
+
+def articulos(request):
+    articulos = Article.objects.all() #order_by('id')[0:1] muestra solo un elemento, funciona igual que una lista
+    articulos = Article.objects.filter(title__contains = 'articulo').exclude( public = False )
+    return render(request, 'mainapp/articulos.html',{
+        'articulos' : articulos
+    })
+
+def borrar_articulo(request, id):
+    articulo = Article.objects.get(pk = id)
+    articulo.delete()
+
+    return redirect('articulos')
+
+def save_article(request):
+    articulo = Article(
+        title = title,
+        content = content, 
+        public = public
+    )
+    articulo.save()
+    return HttpResponse(f'Articulo Creado: {articulo.title}')
+
+def create_article(request):
+    return render(request, 'mainapp/create_article.html')
