4491101d07746e4620cb641279bd502d9b0eed3a
tatianabacik <julieta.bacik@gmail.com>
2021-11-29 19:35:59 -0300

    plantilla index.html y archivos estaticos(img-js-css)

--- ProyectoFinal/settings.py
+++ ProyectoFinal/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os.path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,8 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_URL = ''
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join('static'), ) ## only for windows 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- ProyectoFinal/urls.py
+++ ProyectoFinal/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from mainapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = 'index'),
     path('inicio/', views.index, name = 'inicio')
-    ]
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
